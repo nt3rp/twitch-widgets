240bb64496be63f5eae4658db429a08163165b4d
Nick Terwoord <nicholas.terwoord@shopify.com>
2016-07-14 21:06:51 -0400

    Start making CLI

--- cli.js
+++ cli.js
@@ -1,5 +1,22 @@
-// Note: This automatically kickstarts the server
-var server = require('./server')
+var vorpal = require('vorpal')();
 
-// Run commands
-// server.broadcast(data)
+// TODO: Get achievements from JSON file
+
+vorpal
+  .command('achievement [name]', 'Trigger a named achievement') // .option('-l, --list')
+  .autocomplete(['ending-1', 'ending-2'])
+  .action(function(args, callback) {
+    this.log(args);
+    callback();
+  });
+
+// TODO: Progress events
+// TODO: Generic event
+// TODO: Bake event recording into the server
+
+vorpal
+  .delimiter('twitch-widgets >')
+  .show();
+
+// TODO: Connect to Server
+// TODO: Send events to all clients
