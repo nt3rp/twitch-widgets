551f5a7c7f239ff15fab2fe75e453a8cc4b06d51
Nick Terwoord <nicholas.terwoord@shopify.com>
2016-07-23 11:51:05 -0400

    Connect data flow

--- widgets/progress/progress.js
+++ widgets/progress/progress.js
@@ -1,6 +1,6 @@
 var server = window.location.hostname + ':' + window.location.port;
-
 var connection = window.connection = new WebSocket('ws://'+server+'/events');
+var topic = 'progress';
 
 connection.onopen = function () {};
 
@@ -12,7 +12,15 @@ connection.onerror = function (error) {
 connection.onmessage = function (e) {
   console.log('message');
 
+  // TODO: Check if data!
   var data = JSON.parse(e.data);
+
+  // check if event is in list of subscribed topics
+  // for now, only allow subscribing to one topic
+  if (!data.topics.includes(topic)) {
+    return
+  }
+
   logProgress(data);
 };
 
@@ -24,8 +32,9 @@ var $chart = $('.chart');
 var logProgress = function(data) {
   // Put an indicator on a line
   // Position = (now - start) / (end-start)
-
   // switch on message type. If config message, do that
+  var location = Math.ceil(Math.random()*100) + '%';
+  createIndicator(location, 'cursor.png');
 };
 
 var createIndicator = function(location, image) {
