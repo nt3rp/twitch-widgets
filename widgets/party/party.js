07ec0e4ba20bbcd31bf1b8c2ba0bcd1fb4de9273
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2017-07-20 16:13:41 -0400

    Adds countdown clock to party

--- widgets/party/party.js
+++ widgets/party/party.js
@@ -2,6 +2,15 @@ var server = window.location.hostname + ':' + window.location.port;
 var connection = window.connection = new WebSocket('ws://'+server+'/events');
 var topic = 'party';
 
+var msToTime = function(ms) {
+  var seconds = ms / 1000;
+  var date = new Date(null);
+  date.setSeconds(seconds);
+  var hours = Math.floor(seconds / 3600);
+  var dateStr = hours + date.toISOString().substr(13, 6);
+  return (hours < 10 ? "0" : "") + dateStr;
+};
+
 connection.onopen = function () {
   // Do nothing for now... maybe send something to the server?
 };
@@ -21,14 +30,17 @@ connection.onmessage = function (e) {
   }
 
   switch (msg.type) {
-    default:
+    case "config":
       initialize(msg.data);
+    case "style":
+      break;
   }
 };
 
 var partyList = document.querySelector('#party');
 var templateStr = document.querySelector('#template-player').innerHTML.trim();
-var template = _.template(templateStr, {'variable': 'data'});
+var template = _.template(templateStr, {variable: 'data', time: msToTime});
+var interval;
 
 var strToHtml = function(html) {
   var parser = new DOMParser()
@@ -38,6 +50,7 @@ var strToHtml = function(html) {
 
 var reset = function() {
   document.querySelectorAll('.player').forEach(function(node) {node.parentNode.removeChild(node)});
+  clearInterval(interval);
 };
 
 var initialize = function(party) {
@@ -47,6 +60,18 @@ var initialize = function(party) {
     var html = template(player)
     partyList.appendChild(strToHtml(html));
   });
+
+  interval = setInterval(tick, 1000);
 }
+
+var tick = function() {
+  document.querySelectorAll('.player:not(.inactive) .playtime').forEach(function(node) {
+    var playtime = parseInt(node.dataset.playtime, 10);
+    node.innerHTML = msToTime(playtime);
+    playtime += 1000;
+    node.dataset.playtime = playtime;
+  });
+}
+
 
 // var MENU_STYLES = ['default', 'green', 'blue']
